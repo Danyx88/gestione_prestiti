5f46c291f34863da6f05cf778781ccf250f288cf
Danilo <danilograziano.cs@gmail.com>
2025-01-24 16:50:27 +0100

    Initial commit

---
+++ src/main/java/org/example/catalogo/Book.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Book {
+    private String ISBN;
+    private String title;
+    private int pub;
+    private int Npage;
+    private String author;
+    private String genre;
+}
